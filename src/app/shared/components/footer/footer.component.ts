1d6726945a10c59b78b32ee8d69e2984ad71e061
marian.olar <marian.olar@fortech.ro>
2020-12-02 19:49:17 +0200

    [ECP-8&9] Add Header and Footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  appName: string = "E-Commerce Platform"
+  version: string = '1.0.0';
+  name: string = 'Teo';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
