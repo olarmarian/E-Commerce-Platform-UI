8cdfbb814a8e68877c9bc4b62a180f9af33cb4c8
olarmarian <48599258+olarmarian@users.noreply.github.com>
2020-12-02 19:52:01 +0200

    [ECP-8&9] Add Header and Footer (#2)
    Co-authored-by: marian.olar <marian.olar@fortech.ro>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
