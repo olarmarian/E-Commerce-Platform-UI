b750820ef9d9ebb220f057fa2e738721527bb5de
anabriscan <42339047+anabriscan@users.noreply.github.com>
2021-01-18 16:53:23 +0200

    [ECP-32] Add user icon dropdown

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
