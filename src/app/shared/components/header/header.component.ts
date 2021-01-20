4b4e27a5bc9e86cbaf04209c2ff44fd24916b110
anabriscan <42339047+anabriscan@users.noreply.github.com>
2021-01-20 13:53:42 +0200

    [ECP-31] Searchbar on navigation header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/security/auth.service';
+import {FormControl} from '@angular/forms';
+import {SubSink} from 'subsink';
+import {ProductsService} from '../../../products/products.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  searchBarControl = new FormControl();
+  productNamesToAutocomplete: [];
+  private subs = new SubSink();
+  constructor(private authService: AuthService,
+              private router: Router,
+              private productsService: ProductsService) {}
 
   ngOnInit(): void {}
 
@@ -27,5 +35,16 @@ export class HeaderComponent implements OnInit {
   onLogOutClick(){
     this.authService.logout();
     this.router.navigate(['products']);
+  }
+
+  onProductNameFilterChange() {
+    const productName =  this.searchBarControl.value;
+    this.subs.sink = this.productsService.getProductsByName(productName).subscribe((data) => {
+      this.productNamesToAutocomplete = data.products;
+   });
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule, MatFormFieldModule, MatAutocompleteModule, FormsModule, ReactiveFormsModule, MatInputModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
