2ce504721268c5e2f2fbdff2888931005082242f
marian.olar <marian.olar@fortech.ro>
2021-01-21 12:49:16 +0200

    Extract URLS and labels to constant file

--- src/app/profile/services/profile.service.ts
+++ src/app/profile/services/profile.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { BASE_URL, CORE_URLS } from 'src/app/constants';
 import { ProfileModel } from '../models/profile.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
-  private profileUrl: string = 'http://localhost:3000/profile';
+  private profileUrl: string = BASE_URL + CORE_URLS.PROFILES;
 
   private httpOptions = {
     headers: new HttpHeaders({
